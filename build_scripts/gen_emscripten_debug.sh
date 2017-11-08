853e46af7c34081d4942411dd83d5fe8d52eac11
Peter Hillerström <peter@hstroem.se>
2017-11-08 16:00:26 +0100

    Updated macOS emscripten build scripts

--- build_scripts/gen_emscripten_debug.sh
+++ build_scripts/gen_emscripten_debug.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# Set PhantasyTestbed main directory as working directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+# Delete old build directory and create new one
 rm -rf build_emscripten_debug
 mkdir build_emscripten_debug
 cd build_emscripten_debug
+
+# Generate build files
 cmake .. -DCMAKE_TOOLCHAIN_FILE="$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+
+# Copy index.html file to build directory
 cp ../index.html index.html
--- build_scripts/gen_emscripten_release.sh
+++ build_scripts/gen_emscripten_release.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# Set PhantasyTestbed main directory as working directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+# Delete old build directory and create new one
 rm -rf build_emscripten_release
 mkdir build_emscripten_release
 cd build_emscripten_release
+
+# Generate build files
 cmake .. -DCMAKE_TOOLCHAIN_FILE="$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+
+# Copy index.html file to build directory
 cp ../index.html index.html
