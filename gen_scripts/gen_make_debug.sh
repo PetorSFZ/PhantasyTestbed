e1d5fdbd6e4befc143f134d346781fe4aa73a5df
Peter Hillerström <peter@hstroem.se>
2018-01-26 22:06:41 +0100

    Added more gen scripts

---
+++ gen_scripts/gen_make_debug.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set PhantasyTestbed main directory as working directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+# Delete old build directory and create new one
+rm -rf build_make_debug
+mkdir build_make_debug
+cd build_make_debug
+
+# Generate build files
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+
+# Create resources symlink
+ln -s ../resources resources
---
+++ gen_scripts/gen_make_release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set PhantasyTestbed main directory as working directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+# Delete old build directory and create new one
+rm -rf build_make_release
+mkdir build_make_release
+cd build_make_release
+
+# Generate build files
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+
+# Create resources symlink
+ln -s ../resources resources
