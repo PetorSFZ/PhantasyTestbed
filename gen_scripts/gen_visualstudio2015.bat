aa1e95640c17d3a4e0fedfded2d8e3f8ef842a5d
Peter Hillerström <peter@hstroem.se>
2018-07-04 22:30:48 +0200

    CMake now generates symlink bat script on Windows

--- gen_scripts/gen_visualstudio2015.bat
+++ gen_scripts/gen_visualstudio2015.bat
@@ -1,30 +1,3 @@
-@echo off
-
-: Admin escalation script from: https://sites.google.com/site/eneerge/scripts/batchgotadmin
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------
-
 : Set working directory to location of this file
 cd %dp0
 
@@ -39,14 +12,5 @@ cd build_msvc2015
 : Generate Visual Studio solution
 cmake .. -G "Visual Studio 14 2015 Win64"
 
-: Create resources symlinks
-cd Debug
-mklink /D resources\ ..\..\resources
-
-cd ..\RelWithDebInfo
-mklink /D resources\ ..\..\resources
-
-cd ..\Release
-mklink /D resources\ ..\..\resources
-
-PAUSE
+: Run create_symlinks.bat script
+create_symlinks.bat
