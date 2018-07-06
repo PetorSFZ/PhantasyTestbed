28e3b7765ffeb2297a3fed6582c738f1c338977f
Peter Hillerström <peter@hstroem.se>
2018-07-06 19:38:38 +0200

    Can now use Renderer-CudaRaytracer instead of Renderer-CompatibleGL

---
+++ gen_scripts/gen_visualstudio2015_cuda.bat
@@ -0,0 +1,16 @@
+: Set working directory to location of this file
+cd %dp0
+
+: Set working directory to root of project
+cd ..
+
+: Create build_msvc2015_cuda directory (and delete old one if it exists)
+rmdir /S /Q build_msvc2015_cuda
+mkdir build_msvc2015_cuda
+cd build_msvc2015_cuda
+
+: Generate Visual Studio solution
+cmake .. -G "Visual Studio 14 2015 Win64" -DCUDA_TRACER=TRUE
+
+: Run create_symlinks.bat script
+create_symlinks.bat
