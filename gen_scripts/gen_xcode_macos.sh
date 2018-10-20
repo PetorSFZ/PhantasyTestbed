9030a2be24bd78bf8937b2e48c2af1fc7b2898ae
Peter Hillerström <peter.hillerstrom93@gmail.com>
2018-10-20 15:48:07 +0200

    Changed xcode gen script to specifically create macOS build

--- gen_scripts/gen_xcode_macos.sh
+++ gen_scripts/gen_xcode_macos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Set PhantasyTestbed main directory as working directory
+# Set main directory as working directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
 # Delete old build directory and create new one
-rm -rf build_xcode
-mkdir build_xcode
-cd build_xcode
+rm -rf build_xcode_macos
+mkdir build_xcode_macos
+cd build_xcode_macos
 
 # Generate build files
 cmake .. -GXcode
