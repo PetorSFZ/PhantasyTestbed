2f6cb06a5ea44b63b5770a770078fa681a4b0894
Peter Hillerström <peter.hillerstrom93@gmail.com>
2018-11-25 20:38:42 +0100

    Missed file

--- gen_scripts/gen_visualstudio2017.bat
+++ gen_scripts/gen_visualstudio2017.bat
@@ -13,7 +13,7 @@ mkdir build_msvc2017
 cd build_msvc2017
 
 : Generate Visual Studio solution
-cmake .. -G "Visual Studio 15 2017 Win64" -T v140
+cmake .. -G "Visual Studio 15 2017 Win64"
 
 : Run create_symlinks.bat script
 call create_symlinks.bat
