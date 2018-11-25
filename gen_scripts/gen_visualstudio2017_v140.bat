c81b4d02f7305e657984d82bd7720f3332f14d51
Peter Hillerström <peter.hillerstrom93@gmail.com>
2018-11-25 20:36:48 +0100

    Added build script for pure Visual Studio 2017 build

---
+++ gen_scripts/gen_visualstudio2017_v140.bat
@@ -0,0 +1,21 @@
+: Run script in its own context
+setlocal
+
+: Set working directory to location of this file
+pushd %~dp0
+
+: Set working directory to root of project
+cd ..
+
+: Create build_msvc2017_v140 directory (and delete old one if it exists)
+rmdir /S /Q build_msvc2017_v140
+mkdir build_msvc2017_v140
+cd build_msvc2017_v140
+
+: Generate Visual Studio solution
+cmake .. -G "Visual Studio 15 2017 Win64" -T v140
+
+: Run create_symlinks.bat script
+call create_symlinks.bat
+
+cd ..
